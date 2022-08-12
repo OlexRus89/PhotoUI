eefe929c31874ca960c8d30f4933d9ba9aff24a0
Alexey Svidnitskiy <lesha.svid@yandex.ru>
2022-08-12 15:20:14 +0300

    Initial Commit

---
+++ PhotoUI/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  PhotoUI
+//
+//  Created by Алексей Свидницкий on 12.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoUI/PhotoUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoUIApp.swift
+//  PhotoUI
+//
+//  Created by Алексей Свидницкий on 12.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotoUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
